be311d76222b50d1f9bcbbf136b932e1b93be361
John Kearney <john.d.kearney1@gmail.com>
2020-07-16 11:12:04 -0500

    sucessfully moved the jumbotron to render above the root div via portals

---
+++ src/components/layouts/HeaderBackground.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+
+
+import './HeaderBackground.css';
+
+const HeaderBackground = () => {
+
+    const headerBackground = <Jumbotron fluid id="jumbotron" className="position-relative overflow-hidden d-none d-sm-block"></Jumbotron>;
+    
+    return ReactDOM.createPortal(headerBackground, document.getElementById('headerBackground-hook'));
+}
+
+export default HeaderBackground;
