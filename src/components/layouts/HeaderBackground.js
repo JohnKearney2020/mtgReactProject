90d5616a58ba6ecd52e81e99df661b1af2d27fa1
John Kearney <john.d.kearney1@gmail.com>
2020-07-16 11:42:36 -0500

    header background image no longer uses a bootstrap jumbotron

--- src/components/layouts/HeaderBackground.js
+++ src/components/layouts/HeaderBackground.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+// import { Jumbotron } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 
 
@@ -7,7 +7,8 @@ import './HeaderBackground.css';
 
 const HeaderBackground = () => {
 
-    const headerBackground = <Jumbotron fluid id="jumbotron" className="position-relative overflow-hidden d-none d-sm-block"></Jumbotron>;
+    // const headerBackground = <Jumbotron fluid id="jumbotron" className="position-relative overflow-hidden d-none d-sm-block"></Jumbotron>;
+    const headerBackground = <div id="headerBackgroundImage"></div>
     
     return ReactDOM.createPortal(headerBackground, document.getElementById('headerBackground-hook'));
 }
