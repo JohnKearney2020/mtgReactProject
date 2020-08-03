802630d5285a6ec1cc990f9979b9b208ee90d860
John Kearney <John.d.kearney1@gmail.com>
2020-08-03 17:59:46 -0500

    still troubleshootig why the mobile navbar elements are hidden

--- src/components/layouts/Navbar/Navbar.js
+++ src/components/layouts/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 // import Switch from './Switch';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -14,20 +15,29 @@ const Navbar = () => {
     }
 
     return (
-        <nav className={navbarIsExpanded ? "nav-active" : ""}>
-            <div className="logo">
-                <img src="/images/setSymbol/THB.png" alt="" height="42" width="42"></img>
-                <h4 id="setName">Theros - Beyond Death</h4>
-            </div>
-            <div className="mainNavLinks">
-                <NavLinks />
-            </div>
-            <div onClick={expandNavbarHandler} className={navbarIsExpanded ? "burger toggle" : "burger"}>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-        </nav>
+        <React.Fragment>
+            <SideDrawer show={navbarIsExpanded} onClick={expandNavbarHandler}>
+                <nav className="main-navigation__drawer-nav">
+                    <h1>TEST</h1>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <nav className={navbarIsExpanded ? "nav-active" : ""}>
+                <div className="logo">
+                    <img src="/images/setSymbol/THB.png" alt="" height="42" width="42"></img>
+                    <h4 id="setName">Theros - Beyond Death</h4>
+                </div>
+
+                <div className="mainNavLinks">
+                    <NavLinks />
+                </div>
+                <div onClick={expandNavbarHandler} className={navbarIsExpanded ? "burger toggle" : "burger"}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+            </nav>
+        </React.Fragment>
     )
 }
 
--- src/components/layouts/Navbar/Switch.js
+++ src/components/layouts/Navbar/Switch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './switch.css';
+import './Switch.css';
 
 const Switch = () => {
     return (
