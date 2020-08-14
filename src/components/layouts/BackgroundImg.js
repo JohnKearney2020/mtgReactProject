68ad1e7180f131876884197bfcc9daab7288af69
John Kearney <John.d.kearney1@gmail.com>
2020-08-14 16:24:17 -0500

    moved background image to BaseLayout, probably need to hook it up to the global store

---
+++ src/components/layouts/BackgroundImg.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+
+// import './BackgroundImg.css';
+
+// const BackgroundImg = () => {
+    // return (
+    //     <div>
+            
+    //     </div>
+    // )
+    // return ReactDom.createPortal(<div id="backgroundImg"></div>,document.getElementById('backgroundImg-hook')
+//     )
+// }
+
+// export default BackgroundImg;
--- src/components/layouts/BaseLayout.js
+++ src/components/layouts/BaseLayout.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Header from './Header';
 // import moduleName from './components/layouts/Footer';
 
+// import BackgroundImg from './BackgroundImg';
+import './BaseLayout.css';
+
 const BaseLayout = (props) => {
     return (
         <>
+            <div id="backgroundImg">
+            {/* <BackgroundImg /> */}
             <Header />
                 {props.children}
             {/* <Footer /> */}
+            </div>
         </>
     )
 }
