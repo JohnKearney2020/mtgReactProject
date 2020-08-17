9a37f3eb456320bc43358a8feae1078f8f59ffb8
John Kearney <John.d.kearney1@gmail.com>
2020-08-17 09:57:25 -0500

    header background works, no longer via a portal

--- src/components/layouts/HeaderBackground.js
+++ src/components/layouts/HeaderBackground.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 
 import './HeaderBackground.css';
 
-const HeaderBackground = () => {
+// const HeaderBackground = () => {
 
-    const headerBackground = <div id="headerBackgroundImage"></div>
+//     const headerBackground = <div id="headerBackgroundImage"></div>
     
-    return ReactDOM.createPortal(headerBackground, document.getElementById('headerBackground-hook'));
+//     return ReactDOM.createPortal(headerBackground, document.getElementById('headerBackground-hook'));
+// }
+
+// export default HeaderBackground;
+
+const HeaderBackground = () => {
+    return (
+        <div id="headerBackgroundImage">
+
+        </div>
+    )
 }
 
 export default HeaderBackground;
