8fd3d8c4c086ad993d4e3c39f3c12359d782e670
John Kearney <John.d.kearney1@gmail.com>
2020-08-20 01:12:22 -0500

    got fade out and fade in animations working!

--- src/components/layouts/HeaderBackground.js
+++ src/components/layouts/HeaderBackground.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { CSSTransition } from 'react-transition-group';
 
 import './HeaderBackground.css';
 
@@ -9,31 +8,96 @@ export class HeaderBackground extends Component {
         super(props);
         this.state = {
             filename: "DEFAULT",
-            backgroundChanged: false,
-            animationClassToggle: ""
+            showOldHeader: false,
+            showNewHeader: true,
+            fadeInClassToggle: "",
+            fadeOutClassToggle: "",
+            prevImgFilePath: "DEFAULT"
         }
     }
 
+    // shouldComponentUpdate(nextProps){
+    //     if(this.props.setForBannerBackground !== nextProps.setForBannerBackground) {
+    //         console.log(`In shouldComponentUpdate`);
+
+    //         this.setState({
+    //             animationClassToggle: "headerFadeOut"
+    //         }, () => {
+    //             console.log(`headerFadeOut class added`);
+    //             setTimeout(() => {
+    //                 // return true;
+    //                 this.setState({
+    //                     animationClassToggle: ""
+    //                 }, () => {
+    //                     console.log(`headerFadeOut class removed`);
+    //                     return true;
+    //                 });
+    //                 // return true;
+    //             }, 2510);
+    //         })
+    //         // return true;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+    componentWillUnmount(){
+        console.log(`in component will unmount`);
+    }
+    componentDidMount(){
+        console.log(`in component did mount`);
+    }
+
+    // componentDidUpdate(prevProps){
+    //     if(this.props.setForBannerBackground !== prevProps.setForBannerBackground) {
+    //         console.log(`in the component did update`);
+    //         this.setState({
+    //             fadeInClassToggle: "headerFadeIn",
+    //             prevImgFilePath: prevProps.setForBannerBackground
+    //         }, () => {
+    //             setTimeout(() => {
+    //                 this.setState({
+    //                     fadeInClassToggle: ""
+    //                 })
+    //             }, 1600);
+    //         })
+    //     }
+    // }
     componentDidUpdate(prevProps){
         if(this.props.setForBannerBackground !== prevProps.setForBannerBackground) {
-            console.log(`Header Background has changed!`);
+            console.log(`in the component did update`);
             this.setState({
-                animationClassToggle: "headerFadeIn"
+                fadeOutClassToggle: "headerFadeOut",
+                prevImgFilePath: prevProps.setForBannerBackground,
+                showOldHeader: true,
+                showNewHeader: false
             }, () => {
                 setTimeout(() => {
                     this.setState({
-                        animationClassToggle: ""
+                        fadeOutClassToggle: "",
+                        showOldHeader: false,
+                        showNewHeader: true,
+                        fadeInClassToggle: "headerFadeIn",
                     })
-                }, 1600);
+                }, 1000);
             })
         }
     }
+
     render() {
+        console.log(`in the render function`);
         let backgroundImgPath = require(`../images/banner/${this.props.setForBannerBackground}.jpg`);
+        let prevBackgroundImgPath = require(`../images/banner/${this.state.prevImgFilePath}.jpg`);
         return (
-            // <div id="headerBackgroundImage" style={{ backgroundImage: `url(${backgroundImgPath})`, height: `400px` }}>
-            <div id="headerBackgroundImage" className={this.state.animationClassToggle} style={{ backgroundImage: `url(${backgroundImgPath})`}}>
-            </div>
+            <>
+                {this.state.showOldHeader && 
+                    <div className={`${this.state.fadeOutClassToggle} headerBackgroundImage`} style={{ backgroundImage: `url(${prevBackgroundImgPath})`}}>
+                    </div>
+                }
+                {this.state.showNewHeader &&
+                    <div className={`${this.state.fadeInClassToggle} headerBackgroundImage`} style={{ backgroundImage: `url(${backgroundImgPath})`}}>
+                    </div>
+                }
+            </>
         )
     }
 }
