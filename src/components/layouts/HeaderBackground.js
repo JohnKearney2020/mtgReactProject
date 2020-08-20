d7173fb0fd03b3c76c2bc7db5968dbb402fd37ea
John Kearney <John.d.kearney1@gmail.com>
2020-08-19 23:19:03 -0500

    added fade in animation to body on page load and to header background image when it changes

--- src/components/layouts/HeaderBackground.js
+++ src/components/layouts/HeaderBackground.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import { CSSTransition } from 'react-transition-group';
 
 import './HeaderBackground.css';
 
@@ -7,15 +8,31 @@ export class HeaderBackground extends Component {
     constructor(props){
         super(props);
         this.state = {
-            filename: "DEFAULT"
+            filename: "DEFAULT",
+            backgroundChanged: false,
+            animationClassToggle: ""
         }
     }
 
+    componentDidUpdate(prevProps){
+        if(this.props.setForBannerBackground !== prevProps.setForBannerBackground) {
+            console.log(`Header Background has changed!`);
+            this.setState({
+                animationClassToggle: "headerFadeIn"
+            }, () => {
+                setTimeout(() => {
+                    this.setState({
+                        animationClassToggle: ""
+                    })
+                }, 1600);
+            })
+        }
+    }
     render() {
         let backgroundImgPath = require(`../images/banner/${this.props.setForBannerBackground}.jpg`);
         return (
             // <div id="headerBackgroundImage" style={{ backgroundImage: `url(${backgroundImgPath})`, height: `400px` }}>
-            <div id="headerBackgroundImage" style={{ backgroundImage: `url(${backgroundImgPath})`}}>
+            <div id="headerBackgroundImage" className={this.state.animationClassToggle} style={{ backgroundImage: `url(${backgroundImgPath})`}}>
             </div>
         )
     }
