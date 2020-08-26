fdb25e2fbcdd1587a40532324324e153e4b3a2b9
John Kearney <John.d.kearney1@gmail.com>
2020-08-26 00:54:09 -0500

    flip icon working

--- src/components/layouts/FlipCard.js
+++ src/components/layouts/FlipCard.js
@@ -4,14 +4,25 @@ import './FlipCard.css';
 
 const FlipCard = (props) => {
     const[flipToggleClass, setFlipToggleClass] = useState("");
+    const[hideFrontFlipIconToggle, setHideFrontFlipIconToggle] = useState("");
+    // const[hideBackFlipIconToggle, setHideBackFlipIconToggle] = useState("");
 
     let imgUrlFront = props.cardObject.card_faces[0].image_uris.normal;
     let imgUrlBack = props.cardObject.card_faces[1].image_uris.normal;
     
     // let flipToggleClass = "";
     const toggleFlipAnimation = () => {
+        console.log(`toggleFlipAnimation function called`);
+        // console.log(`hideFrontFlipIconToggle1: ${hideFrontFlipIconToggle}`);
+        // console.log(`hideBackFlipIconToggle1: ${hideBackFlipIconToggle}`);
         // var beverage = (age >= 21) ? "Beer" : "Juice";
         (flipToggleClass === "flip-toggle") ? setFlipToggleClass("") : setFlipToggleClass("flip-toggle");
+        (hideFrontFlipIconToggle === "hideFlipIcon") ? setHideFrontFlipIconToggle("") : setHideFrontFlipIconToggle("hideFlipIcon");
+        // (hideBackFlipIconToggle === "") ? setHideBackFlipIconToggle("hideFlipIcon") : setHideFrontFlipIconToggle("");
+        // (hideBackFlipIconToggle === "hideFlipIcon") ? setHideBackFlipIconToggle("") : setHideFrontFlipIconToggle("hideFlipIcon");
+
+
+        // setHideFlipIconToggle("hideFlipIcon");
         // setTimeout(() => {
         //     setFlipToggleClass("");
         // }, 5000);
@@ -24,8 +35,8 @@ const FlipCard = (props) => {
                         {/* <div className="flipCardContainer"> */}
                             <img className="flipCardImg" src={imgUrlFront} alt="Avatar"></img>
                             <div onClick={toggleFlipAnimation}>
-                                <span className="fa-stack fa-2x frontFlipIcon">
-                                    <i class="fas fa-circle fa-stack-1x"></i>
+                                <span className={`fa-stack fa-2x frontFlipIcon ${hideFrontFlipIconToggle}`}>
+                                    <i className="fas fa-circle fa-stack-1x"></i>
                                     <i className="fab fa-creative-commons-share fa-stack-1x"></i>
                                 </span>
                             </div>
@@ -41,7 +52,12 @@ const FlipCard = (props) => {
                     <div className="flip-card-back">
                         <img className="flipCardImg" src={imgUrlBack} alt="Avatar"></img>
                         <div onClick={toggleFlipAnimation}>
-                                <i className="fab fa-creative-commons-share"></i>
+                                {/* <i className="fab fa-creative-commons-share"></i> */}
+                                {/* <span className={`fa-stack fa-2x backFlipIcon ${hideBackFlipIconToggle}`}> */}
+                                <span className={`fa-stack fa-2x backFlipIcon`}>
+                                    <i className="fas fa-circle fa-stack-1x"></i>
+                                    <i className="fab fa-creative-commons-share fa-stack-1x"></i>
+                                </span>
                         </div>
                     </div>
                 </div>
