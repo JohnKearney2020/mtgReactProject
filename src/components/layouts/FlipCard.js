503108410df20b57f46514fcfe849a747a594a1c
John Kearney <john.d.kearney1@gmail.com>
2021-06-25 12:15:12 -0500

    implemented a temporary workaround for the app crashing b/c forgotten realms art series cards do not have images yet. A long term solution will involve use of the .image_status property of the card object

--- src/components/layouts/FlipCard.js
+++ src/components/layouts/FlipCard.js
@@ -5,9 +5,14 @@ import './FlipCard.css';
 const FlipCard = (props) => {
     const[flipToggleClass, setFlipToggleClass] = useState("");
     const[hideFrontFlipIconToggle, setHideFrontFlipIconToggle] = useState("");
-
-    let imgUrlFront = props.cardObject.card_faces[0].image_uris.normal;
-    let imgUrlBack = props.cardObject.card_faces[1].image_uris.normal;
+    console.log('in flip card:');
+    console.log(props.cardObject);
+    // let imgUrlFront = props.cardObject.card_faces[0].image_uris.normal || "https://i.imgur.com/Qx6AMQQ.jpg";
+    let imgUrlFront = props.cardObject.card_faces[0].hasOwnProperty('image_uris') ? props.cardObject.card_faces[0].image_uris.normal : "https://i.imgur.com/Qx6AMQQ.jpg";
+    
+    // let imgUrlFront = props.cardObject.card_faces[0].image_uris.normal ? props.cardObject.card_faces[0].image_uris.normal : "https://i.imgur.com/Qx6AMQQ.jpg";
+    // let imgUrlBack = props.cardObject.card_faces[1].image_uris.normal || "https://i.imgur.com/Qx6AMQQ.jpg";
+    let imgUrlBack = props.cardObject.card_faces[1].hasOwnProperty('image_uris') ? props.cardObject.card_faces[1].image_uris.normal : "https://i.imgur.com/Qx6AMQQ.jpg";
     
     const toggleFlipAnimation = () => {
         (flipToggleClass === "flip-toggle") ? setFlipToggleClass("") : setFlipToggleClass("flip-toggle");
@@ -28,8 +33,10 @@ const FlipCard = (props) => {
                             data-cmc={props.cardObject.cmc}
                             data-front_flavor_text={props.cardObject.card_faces[0].flavor_text}
                             data-back_flavor_text={props.cardObject.card_faces[1].flavor_text}
-                            data-front_image_url={props.cardObject.card_faces[0].image_uris.border_crop}
-                            data-back_image_url={props.cardObject.card_faces[1].image_uris.border_crop}
+                            // data-front_image_url={props.cardObject.card_faces[0].image_uris.border_crop}
+                            data-front_image_url={imgUrlFront}
+                            // data-back_image_url={props.cardObject.card_faces[1].image_uris.border_crop}
+                            data-back_image_url={imgUrlBack}
                             data-mana_cost={props.cardObject.card_faces[0].mana_cost}
                             data-front_oracle_text={props.cardObject.card_faces[0].oracle_text}
                             data-back_oracle_text={props.cardObject.card_faces[1].oracle_text}
@@ -65,8 +72,10 @@ const FlipCard = (props) => {
                             data-cmc={props.cardObject.cmc}
                             data-front_flavor_text={props.cardObject.card_faces[0].flavor_text}
                             data-back_flavor_text={props.cardObject.card_faces[1].flavor_text}
-                            data-front_image_url={props.cardObject.card_faces[0].image_uris.border_crop}
-                            data-back_image_url={props.cardObject.card_faces[1].image_uris.border_crop}
+                            // data-front_image_url={props.cardObject.card_faces[0].image_uris.border_crop}
+                            data-front_image_url={imgUrlFront}
+                            // data-back_image_url={props.cardObject.card_faces[1].image_uris.border_crop}
+                            data-back_image_url={imgUrlBack}
                             data-mana_cost={props.cardObject.card_faces[0].mana_cost}
                             data-front_oracle_text={props.cardObject.card_faces[0].oracle_text}
                             data-back_oracle_text={props.cardObject.card_faces[1].oracle_text}
